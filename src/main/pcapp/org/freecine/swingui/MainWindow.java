b027b9aa9aebc6a13b26db50719270eca8a01edf
Harri Kaimio <harri@kaimio.fi>
2008-03-29 17:33:02 +0200

    Relicensed under GPLv3

--- src/main/pcapp/org/freecine/swingui/MainWindow.java
+++ src/main/pcapp/org/freecine/swingui/MainWindow.java
@@ -1,7 +1,26 @@
 /*
- * MainWindow.java
- *
- * Created on 21. maaliskuuta 2008, 11:11
+Copyright (C) 2008 Harri Kaimio
+ 
+This file is part of Freecine
+ 
+Freecine is free software; you can redistribute it and/or modify it 
+under the terms of the GNU General Public License as published by the Free 
+Software Foundation; either version 3 of the License, or (at your option) 
+any later version.
+ 
+This program is distributed in the hope that it will be useful, but WITHOUT 
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS 
+FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+You should have received a copy of the GNU General Public License along with 
+this program; if not, see <http://www.gnu.org/licenses>.
+ 
+Additional permission under GNU GPL version 3 section 7
+ 
+If you modify this Program, or any covered work, by linking or combining it 
+with Java Advanced Imaging (or a modified version of that library), containing 
+parts covered by the terms of Java Distribution License, or leJOS, containing 
+parts covered by the terms of Mozilla Public License, the licensors of this 
+Program grant you additional permission to convey the resulting work. 
  */
 
 package org.freecine.swingui;
